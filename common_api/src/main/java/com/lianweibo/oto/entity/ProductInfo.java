19fda857ba9d55de6b6972ac93ced86dd0116a3c
lwb <2062626143@qq.com>
2019-11-13 20:24:34 +0800

    oto 项目业务初始化

---
+++ common_api/src/main/java/com/lianweibo/oto/entity/ProductInfo.java
@@ -0,0 +1,13 @@
+package com.lianweibo.oto.entity;
+
+/**
+ * @ClassName ProductInfo
+ * @Description: TODO
+ * @Author :lianweibo
+ * @Date 2019/11/13
+ * @Version V1.0
+ **/
+
+public class ProductInfo {
+
+}
---
+++ eureka_server/src/main/java/com/lianweibo/oto/OtoEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.lianweibo.oto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName OtoEurekaApplication
+ * @Description: TODO
+ * @Author :lianweibo
+ * @Date 2019/11/13
+ * @Version V1.0
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class OtoEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtoEurekaApplication.class,args);
+    }
+}
